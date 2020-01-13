b1159a4a6f8ceed3e8369263ec93405c50b40d32
wei <wqz9340@163.com>
2020-01-13 14:19:01 +0800

    test2

--- src/hiworld.java
+++ src/hiworld.java
@@ -1,7 +1,5 @@
 public class hiworld {
     public static void main(String args[]){
         System.out.println("hello world");
-        System.out.println("hello world1");
-        System.out.println("hello world2");
     }
 }
